e3310db14a5af88cb1e07bfbfe530f96a8eaa96b
vampiredk2 <simonbreit2@gmail.com>
2022-05-19 11:14:11 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+    <div id="topbar">
+      <div id="topbarcolour">
+      </div>
+      <div class="topbarcontent">
+        <div class="topbarcontentpos">
+          <router-link to="/">
+            <button class="topbarbtn">Home</button>
+          </router-link>
+          <router-link to="/about">
+            <button class="topbarbtn">about</button>
+          </router-link>
+          <div class="topbarcontentright">
+            <button class="topbarbtn">kurv</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+</style>
